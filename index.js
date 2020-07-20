398ba18a44bd09c1a661e4d34be68582f1772da5
James Culveyhouse <jculveyhouse@cloudflare.com>
2020-07-20 15:37:12 -0700

    Fixes

--- index.js
+++ index.js
@@ -140,11 +140,8 @@ const html = `<!doctype html>
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
-async function handleRequest(request) {
+
+async function handleRequest() {
   return new Response(html, {
     headers: { 'content-type': 'text/html' },
   })
